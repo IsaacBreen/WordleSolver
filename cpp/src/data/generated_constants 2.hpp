2c76dbdb72c3a75ee865dfce858724fea51cfdb5
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-09-03 14:37:29 +0800

    gut everything

--- cpp/src/data/generated_constants 2.hpp
+++
@@ -1,3 +0,0 @@
-constexpr int WORD_LENGTH = 5;
-const int NUM_WORDS = 2309;
-const int NUM_GUESSES = 12947;
--- cpp/test/test_hint 2.cpp
+++
@@ -1,6 +0,0 @@
-#include <hint.hpp>
-
-constexpr void test_hints() {
-    static_assert(make_hint("aaaaa","bbbbb") == 0);
-    static_assert(make_hint("aabbc", "dbbba") == string_to_hint("bbggy"));
-}
